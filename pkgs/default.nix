5ae2b7f3691364ba17cdb01fde98c2722d3a707e
tv <tv@krebsco.de>
2018-09-16 10:10:37 +0200

    pkgs: add nix-writers 3.0.0 to overlays

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,15 @@
 { overlays ? [], ... }@args:
 
+let
+  nix-writers = builtins.fetchGit {
+    url = https://cgit.krebsco.de/nix-writers/;
+    rev = "c27a9416e8ee04d708b11b48f8cf1a055c0cc079";
+  };
+in
+
 import <nixpkgs> (args // {
   overlays = overlays ++ [
     (import ./overlay.nix)
+    (import "${nix-writers}/pkgs")
   ];
 })
