219e5aa008e16d01ebaf76e71f08c82b3c9fb740
tv <tv@krebsco.de>
2019-04-02 20:12:13 +0200

    pkgs nix-writers: 3.0.0 -> 3.4.0

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@
 let
   nix-writers = builtins.fetchGit {
     url = https://cgit.krebsco.de/nix-writers/;
-    rev = "c27a9416e8ee04d708b11b48f8cf1a055c0cc079";
+    rev = "c528cf970e292790b414b4c1c8c8e9d7e73b2a71";
   };
 in
 
